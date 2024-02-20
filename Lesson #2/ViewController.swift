c2d43259cc09c2eaa4cd35f7143ef93be9e4d08b
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2024-02-20 21:27:50 +0300

    Initial Commit

---
+++ Lesson #2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson #2
+//
+//  Created by Захар Литвинчук on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
